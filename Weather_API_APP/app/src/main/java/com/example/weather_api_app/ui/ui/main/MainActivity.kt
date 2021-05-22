e078d9c3c45de0df0e32f37ab0af35d2bcdd0760
Shrouk92 <shrouk.ghania@gmail.com>
2021-05-22 12:17:49 +0200

    more modifications

--- Weather_API_APP/app/src/main/java/com/example/weather_api_app/ui/ui/main/MainActivity.kt
+++ Weather_API_APP/app/src/main/java/com/example/weather_api_app/ui/ui/main/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.weather_api_app.ui.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.weather_api_app.R
 import com.example.weather_api_app.databinding.ActivityMainBinding
@@ -17,10 +20,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-       W_ViewModel=ViewModelProviders.of(this).get(WeatherViewModel::class.java)
+       W_ViewModel= ViewModelProvider(this).get(WeatherViewModel::class.java)
         Binding.weather=W_ViewModel
         Binding.lifecycleOwner=this
         Binding.root
+        Binding.b.isVisible=false
+        Binding.constraintLayout.isVisible=false
+        Binding.imageButton.setOnClickListener(View.OnClickListener {
+            W_ViewModel.get_Weather()
+            W_ViewModel.api_response.observe(this,{newRespon
+            ->
+                Binding.b.isVisible=true
+                Binding.constraintLayout.isVisible=true
+                Binding.textView.text=W_ViewModel.City +","+newRespon.sys.country
+                Binding.textView3.text = newRespon.main.temp.toString() +"°C"
+                Binding.minTemp.text=newRespon.main.temp_min.toString()+"°C"
+                Binding.maxTemp.text=newRespon.main.temp_max.toString()+"°C"
+                Binding.FLike.text= "Feels Like :"+newRespon.main.temp.toString()+"°C"
+                Binding.textView7.text=newRespon.weather[0].main
+                Binding.textView4.text=newRespon.main.humidity.toString()+"%"
+                Binding.textView5.text=newRespon.main.pressure.toString() +"HPa"
+                Binding.textView6.text=newRespon.wind.toString()+" meter/sec"
+                Binding.textView8.text=newRespon.clouds.all.toString()+"%"
+
+
+            })
+
+        })
+
+
 
 
 
--- Weather_API_APP/app/src/main/java/com/example/weather_api_app/ui/ui/main/WeatherViewModel.kt
+++ Weather_API_APP/app/src/main/java/com/example/weather_api_app/ui/ui/main/WeatherViewModel.kt
@@ -1,24 +1,42 @@
 package com.example.weather_api_app.ui.ui.main
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.weather_api_app.Data.WAPI
 import com.example.weather_api_app.response.Currentweather
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 private const val Appid:String="fe7d59c0780f5cdce09f843250d3c11e"
 private const val units:String="metric"
 
 class WeatherViewModel :ViewModel()
 {
 
-     var City = MutableLiveData<String>()
-    private val API_Response=MutableLiveData<Currentweather>()
+    var City:String = ""
 
-    init {
-        get_Weather()
-    }
-     fun get_Weather(  )
+    private val API_Response=MutableLiveData<Currentweather>()
+    val api_response:LiveData<Currentweather>
+        get()=API_Response
+
+
+
+    public fun get_Weather()
     {
-        WAPI.API_service.get_current_weather(City.toString(),Appid , units)
+        val C:Call<Currentweather> = WAPI.API_service.get_current_weather(City,Appid , units)
+        C.enqueue( object : Callback<Currentweather> {
+
+            override fun onResponse(call: Call<Currentweather>, response: Response<Currentweather>) {
+                API_Response.value=response.body()
+            }
+
+            override fun onFailure(call: Call<Currentweather>, t: Throwable) {
+                Log.d("sdnc","sds")
+            }
+        })
+
     }
 }
