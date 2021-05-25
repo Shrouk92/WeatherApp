e5d74a4d1339f49894901d490af244533c8c56c3
Shrouk92 <shrouk.ghania@gmail.com>
2021-05-26 00:59:41 +0200

    cleaning more code from activity

--- Weather_API_APP/app/src/main/java/com/example/weather_api_app/ui/ui/main/MainActivity.kt
+++ Weather_API_APP/app/src/main/java/com/example/weather_api_app/ui/ui/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
@@ -11,6 +12,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.weather_api_app.R
 import com.example.weather_api_app.databinding.ActivityMainBinding
+import java.lang.Exception
+import java.lang.NullPointerException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -24,57 +27,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
-        val current = LocalDateTime.now()
-
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd   HH:mm")
-        val formatted = current.format(formatter)
-
-
-
         W_ViewModel= ViewModelProvider(this).get(WeatherViewModel::class.java)
         Binding.weather=W_ViewModel
         Binding.lifecycleOwner=this
         Binding.root
-        Binding.constraintLayout.isVisible=false
-        Binding.constraintLayout.animate().translationY(100F)
-        Binding.imageButton.setOnClickListener(View.OnClickListener {
-            W_ViewModel.get_Weather()
-            W_ViewModel.api_response.observe(this,{newRespon
-            ->
-                Binding.constraintLayout.isVisible=true
-                Binding.textView10.text=formatted
-                Binding.textView.text=W_ViewModel.City +","+newRespon.sys.country
-                Binding.textView3.text = newRespon.main.temp.toString() +"°C"
-                Binding.minTemp.text=newRespon.main.temp_min.toString()+"°C"
-                Binding.maxTemp.text=newRespon.main.temp_max.toString()+"°C"
-                Binding.FLike.text= "Feels Like :"+newRespon.main.temp.toString()+"°C"
-                Binding.textView7.text=newRespon.weather[0].main
-                Binding.textView4.text=newRespon.main.humidity.toString()+"%"
-                Binding.textView5.text=newRespon.main.pressure.toString() +"HPa"
-                Binding.textView6.text=newRespon.wind.speed
-                        .toString()+" meter/sec"
-                Binding.textView8.text=newRespon.clouds.all.toString()+"%"
-
-
-            })
-
-        })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 }
--- Weather_API_APP/app/src/main/java/com/example/weather_api_app/ui/ui/main/WeatherViewModel.kt
+++ Weather_API_APP/app/src/main/java/com/example/weather_api_app/ui/ui/main/WeatherViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.weather_api_app.ui.ui.main
 
+import android.content.Context
+import android.os.Build
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.databinding.ObservableField
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,6 +16,9 @@ import com.example.weather_api_app.response.Currentweather
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 
 private const val Appid:String="fe7d59c0780f5cdce09f843250d3c11e"
 private const val units:String="metric"
@@ -16,27 +26,79 @@ private const val units:String="metric"
 class WeatherViewModel :ViewModel()
 {
 
+    val TempratureObserver=ObservableField<String>()
+     val ResponseObserver=ObservableField<Currentweather>()
+    val CountryObserver=ObservableField<String>()
+    val TempratureMaxObserver=ObservableField<String>()
+    val TempratureMinObserver=ObservableField<String>()
+    val MainWeatherObserver=ObservableField<String>()
+    val FeelsLikeObserver=ObservableField<String>()
+    val HumidityObserver=ObservableField<String>()
+    val PressureObserver=ObservableField<String>()
+    val windObserver=ObservableField<String>()
+    val cloudsObserver=ObservableField<String>()
+    val TimeObserver=ObservableField<String>()
+
+
     var City:String = ""
 
     private val API_Response=MutableLiveData<Currentweather>()
     val api_response:LiveData<Currentweather>
         get()=API_Response
+    @RequiresApi(Build.VERSION_CODES.O)
+    val current = LocalDateTime.now()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd   HH:mm")
+    @RequiresApi(Build.VERSION_CODES.O)
+    val formatted = current.format(formatter)
 
 
-
-    public fun get_Weather()
+    public fun get_Weather(view:View)
     {
         val C:Call<Currentweather> = WAPI.API_service.get_current_weather(City,Appid , units)
         C.enqueue( object : Callback<Currentweather> {
 
             override fun onResponse(call: Call<Currentweather>, response: Response<Currentweather>) {
                 API_Response.value=response.body()
+                if(API_Response.value!=null){
+                    TempratureObserver.set(API_Response.value?.main?.temp.toString()+"°C")
+                    CountryObserver.set(API_Response.value?.sys?.country)
+                    TempratureMaxObserver.set(API_Response.value?.main?.temp_max.toString()+"°C")
+                    TempratureMinObserver.set(API_Response.value?.main?.temp_min.toString()+"°C")
+                    MainWeatherObserver.set(API_Response.value?.weather?.get(0)?.description)
+                    FeelsLikeObserver.set("Feels Like : "+API_Response.value?.main?.temp.toString()+"°C")
+                    HumidityObserver.set(API_Response.value?.main?.humidity.toString()+"%")
+                    PressureObserver.set(API_Response.value?.main?.pressure.toString() +"HPa")
+                    windObserver.set(API_Response.value?.wind?.speed.toString()+" meter/sec")
+                    cloudsObserver.set(API_Response.value?.clouds?.all.toString()+"%")
+                    TimeObserver.set(formatted)
+                }
+                else
+                {
+                    Toast.makeText(view.context,"Invalid Country",Toast.LENGTH_LONG).show()
+
+                }
+
+
             }
 
             override fun onFailure(call: Call<Currentweather>, t: Throwable) {
-                Log.d("sdnc","sds")
+               // Log.d("sdnc","sds")
+
             }
         })
-
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
